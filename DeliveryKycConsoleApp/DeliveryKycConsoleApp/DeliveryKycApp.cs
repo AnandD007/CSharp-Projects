2be33cf396454ac5000160bcb718a71953216417
Anand-Dev <64485733+AnandD007@users.noreply.github.com>
2023-02-14 19:41:10 +0530

    Updated new changes in Delivery KYC Application Code

--- DeliveryKycConsoleApp/DeliveryKycConsoleApp/DeliveryKycApp.cs
+++ DeliveryKycConsoleApp/DeliveryKycConsoleApp/DeliveryKycApp.cs
@@ -1,12 +1,5 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
+
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace DeliveryKycConsoleApp
 {
@@ -38,83 +31,76 @@ namespace DeliveryKycConsoleApp
             form.Address = "";
             form.MiddleName = "";
             form.GovernmentId = "";
-            string firstName = form.FirstName;
-            string lastName = form.LastName;
-            string emailId = form.Email;
-            string middleName = form.MiddleName;
-            int ageRange = form.Age;
-            long phoneNumber = form.PhoneNumber;
-            string address = form.Address;
-            string governmentId = form.GovernmentId;
-            string governmentIdName = form.GovernmentIdName;
             Regex regexForAlphaValue = new Regex(@"[\d]");
-            Regex regexForPhoneNumber = new Regex(@"[\d]$");
+            Regex regexForPhoneNumber = new Regex(@"^-?[0-9][0-9,\.]+$");
 
             Console.WriteLine("\n================+-+-+-+-+-+WELCOME TO DELIVERION+-+-+-+-+-+================\n\n");
-            // new user entry
-            Console.WriteLine("                       Enter Shipper Personal Details:\n");
+            Console.WriteLine("  Shipper Registration Form\n\n");
+            Console.WriteLine("  Enter Shipper Personal Details:\n\n");
 
-            Console.Write("\nEnter First Name:");
+            Console.Write("Enter First Name:");
         gotoFirstName:
-            firstName = Console.ReadLine();  // You need to enter fname second time only if our validations throws error for input..!
-                                             // Content "" validation checking
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrWhiteSpace(firstName))
+            form.FirstName = Console.ReadLine();  // You need to enter fname second time only if our validations throws error for input..!
+
+            if (string.IsNullOrEmpty(form.FirstName) || string.IsNullOrWhiteSpace(form.FirstName))
             {
                 Console.Write("\nFirst name should be not null..!\n\nEnter First Name:");
                 goto gotoFirstName;
             }
-            if (regexForAlphaValue.IsMatch(firstName))
+            if (regexForAlphaValue.IsMatch(form.FirstName))
             {
                 Console.Write("\nFirst Name letters should be alphabets...!\n\nEnter First Name:");
                 goto gotoFirstName;
             }
-            Console.Write("\nEnter Middle Name(Optional):");
-            middleName = Console.ReadLine();
+            Console.Write("\n\nEnter Middle Name(Optional):");
+            form.MiddleName = Console.ReadLine();
 
-            Console.Write("\n\nEnter Last Name:");
+            Console.Write("\n\nEnter the Last Name:");
         gotoLastName:
-            lastName = Console.ReadLine();
-            if (string.IsNullOrEmpty(lastName) || string.IsNullOrWhiteSpace(lastName))
+            form.LastName = Console.ReadLine();
+            if (string.IsNullOrEmpty(form.LastName) || string.IsNullOrWhiteSpace(form.LastName))
             {
-                Console.Write("\nLast name should not be null..!\n\nEnter List Name:");
+                Console.Write("\nLast name should not be null..!\n\nEnter the Last Name:");
                 goto gotoLastName;
             }
-            else if (regexForAlphaValue.IsMatch(lastName))
+            else if (regexForAlphaValue.IsMatch(form.LastName))
             {
-                Console.Write("\nLast Name letters should be alphabets...!\n\nEnter First Name:");
+                Console.Write("\nLast Name letters should be alphabets...!\n\nEnter the Last Name:");
                 goto gotoLastName;
             }
-
-            Console.Write("\n\nEnter Email ID:");
+            Console.Write("\n\nEnter the Email ID:");
         gotoEmailId:
-            emailId = Console.ReadLine().ToLower();
+            form.Email = Console.ReadLine().ToLower();
 
-            Console.WriteLine((emailId).Length);
-
-            if (string.IsNullOrEmpty(emailId) || string.IsNullOrWhiteSpace(emailId))
+            if (string.IsNullOrEmpty(form.Email) || string.IsNullOrWhiteSpace(form.Email))
             {
-                Console.Write("\nEmail Id  should not be null..!\n\nEnter Email Id:");
+                Console.Write("\nThe Email Id should not be null..!\n\nEnter the Email Id:");
                 goto gotoEmailId;
             }
-            else if (!(emailId.EndsWith("@outlook.com") || emailId.EndsWith("@gmail.com") || emailId.EndsWith("@incubxperts.com")))
+            else if (!(form.Email.EndsWith("@outlook.com") || form.Email.EndsWith("@gmail.com") || form.Email.EndsWith("@incubxperts.com")))
             {
-                Console.Write("\nEnter Correct Email Id Domain...!\nEmail Id Domains(gmail,outlook,incubxperts)\n\nEnter Email Id:");
+                Console.Write("\nEnter the Correct Email Id Domain...!\nEmail Id Domains(gmail,outlook,incubxperts)\n\nEnter Email Id:");
                 goto gotoEmailId;
-            }
-            // ---------------------------------------------------------------                
+            }           
 
             Console.Write("\n\nEnter the Age:");
 
         AgeFieldChecker:
             string age = Console.ReadLine();
-            if (!(regexForPhoneNumber.IsMatch(age)))
+            if (!(regexForAlphaValue.IsMatch(age)))
             {
                 Console.Write("\nAll the values should be numeric..!\n\nEnter the Age:");
                 goto AgeFieldChecker;
             }
             else
             {
-                int AgeRange = Convert.ToInt32(age);
+                form.Age = Convert.ToInt32(age);
+                if (!(form.Age >= 18) || (form.Age >= 120))
+                {
+                    Console.WriteLine("\nAccording to Shipper Account Age Limits. You are not eligible to become Shipper...!\n");
+                    Console.WriteLine("         Thank You For Using Our Service             \n\n");
+                    Environment.Exit(0);
+                }
             }
             if (string.IsNullOrWhiteSpace(Convert.ToString(age)) || string.IsNullOrEmpty(Convert.ToString(age)))
             {
@@ -122,78 +108,97 @@ namespace DeliveryKycConsoleApp
                 goto AgeFieldChecker;
             }
 
-            Console.Write("\n\nEnter Phone Number:");
+            Console.Write("\n\nEnter the Phone Number:");
 
         phoneNumberAdder:
             string phoneNumbercheker = Console.ReadLine();
             if (!(regexForPhoneNumber.IsMatch(phoneNumbercheker)))
             {
-                Console.Write("\nAll the values should be numeric..!\n\nEnter Phone Number:");
+                Console.Write("\nAll the values should be numeric..!\n\nEnter the Phone Number:");
                 goto phoneNumberAdder;
             }
             else
             {
-                phoneNumber = Convert.ToInt64(phoneNumbercheker);
+                form.PhoneNumber = Convert.ToInt64(phoneNumbercheker);
             }
-            if (string.IsNullOrWhiteSpace(Convert.ToString(phoneNumber)) || string.IsNullOrEmpty(Convert.ToString(phoneNumber)))
+            if (string.IsNullOrWhiteSpace(Convert.ToString(form.PhoneNumber)) || string.IsNullOrEmpty(Convert.ToString(form.PhoneNumber)))
             {
-                Console.Write("\nInvalid Phone Number should not be null..!\n\nEnter Phone Number:");
+                Console.Write("\nInvalid Phone Number should not be null..!\n\nEnter the Phone Number:");
                 goto phoneNumberAdder;
             }
-            if ((Convert.ToString(phoneNumber)).Length == 12)
+            if ((Convert.ToString(form.PhoneNumber)).Length == 12)
             {
-                Console.Write("\nDon't add the country code in phone number..!\n\nEnter Phone Number:");
+                Console.Write("\nDon't add the country code in phone number..!\n\nEnter the Phone Number:");
                 goto phoneNumberAdder;
             }
-            else if ((Convert.ToString(phoneNumber)).Length != 10)
+            else if ((Convert.ToString(form.PhoneNumber)).Length != 10)
             {
-                Console.Write("\nInvalid Phone Number Entered..!\n\nEnter Phone Number:");
+                Console.Write("\nInvalid Phone Number Entered..!\n\nEnter the Phone Number:");
                 goto phoneNumberAdder;
             }
 
             Console.WriteLine("\n\n1. Permanent Account Number (PAN)\n2. Aadhar Card\n3. Driving License");
             Console.Write("\n\nEnter Government Id Name:");
-            int choice = 10;
         governmentIdNameChecker:
-            governmentIdName = Console.ReadLine();
+            form.GovernmentIdName = Console.ReadLine();
 
-            if (string.IsNullOrEmpty(governmentIdName) || string.IsNullOrWhiteSpace(governmentIdName))
+            if (string.IsNullOrEmpty(form.GovernmentIdName) || string.IsNullOrWhiteSpace(form.GovernmentIdName))
             {
                 Console.Write("\nChoose Correct Government Id Name. Field should not be null..!\n\nEnter Choose the option number assigned Governmemnt Id Name (1/2/3):");
                 goto governmentIdNameChecker;
             }
-            else if (!(regexForPhoneNumber.IsMatch(governmentIdName)))
+            else if (!(regexForAlphaValue.IsMatch(form.GovernmentIdName)))
             {
                 Console.Write("\nGovernment Id Name Choice should be numeric...!\n\nEnter Choose the option number assigned Governmemnt Id Name (1/2/3):");
                 goto governmentIdNameChecker;
             }
-            Console.Write("\n\nEnter the Government Id Number:");
-        governmentIdChecker:
-            governmentId = Console.ReadLine();
-            if (string.IsNullOrEmpty(lastName) || string.IsNullOrWhiteSpace(lastName))
+
+            if (form.GovernmentIdName == "1")
             {
-                Console.Write("\nLast name should not be null..!\n\nEnter the Government Id Number:");
+                Console.Write("\nEnter the Permanent Account Number:");
+                form.SetGovernmentIdName = "Permanent Account Number (PAN)";
+            }
+            else if(form.GovernmentIdName == "2")
+            {
+                Console.Write("\nEnter the Aadhar Number:");
+                form.SetGovernmentIdName = "Aadhar Card";
+            }
+            else if(form.GovernmentIdName == "3")
+            {
+                Console.Write("\nEnter the Driving License:");
+                form.SetGovernmentIdName = "Driving License";
+            }
+        governmentIdChecker:
+            form.GovernmentId = Console.ReadLine();
+            if (string.IsNullOrEmpty(form.GovernmentId) || string.IsNullOrWhiteSpace(form.GovernmentId))
+            {
+                Console.Write("\nGovernment Id should not be null..!\n\nEnter the Government Id Number:");
                 goto governmentIdChecker;
             }
-            if (governmentIdName == "2")
+            if (form.GovernmentIdName == "2")
             {
-                if (!(governmentId.Length == 12))
+                if (!(form.GovernmentId.Length == 12))
+                {
+                    Console.Write("\nInvalid Government Id...!\n\nEnter the Government Id Number:");
+                    goto governmentIdChecker;
+                }
+                if(!(regexForPhoneNumber.IsMatch(form.GovernmentId)))
+                {
+                    Console.WriteLine("Aadhar Card Number should be numeric...!\n\nEnter the Aadhar Card Number:");
+                    goto governmentIdChecker;
+                }
+            }
+            if (form.GovernmentIdName == "1")
+            {
+                if (!(form.GovernmentId.Length == 10))
                 {
                     Console.Write("\nInvalid Government Id...!\n\nEnter the Government Id Number:");
                     goto governmentIdChecker;
                 }
             }
-            if (governmentIdName == "1")
+            if (form.GovernmentIdName == "3")
             {
-                if (!(governmentId.Length == 10))
-                {
-                    Console.Write("\nInvalid Government Id...!\n\nEnter the Government Id Number:");
-                    goto governmentIdChecker;
-                }
-            }
-            if (governmentIdName == "3")
-            {
-                if (!(governmentId.Length == 15))
+                if (!(form.GovernmentId.Length == 15))
                 {
                     Console.Write("\nInvalid Government Id...!\n\nEnter the Government Id Number:");
                     goto governmentIdChecker;
@@ -201,28 +206,27 @@ namespace DeliveryKycConsoleApp
             }
 
         // Address Entry & Validation
-            Console.Write("\n\nEnter Your Permanent Address (As Per Govt Id Proof):");
-            
+            Console.Write("Enter Your Full Permanent Address (As Per Govt Id Proof):");
         addressChecker:
-            address = Console.ReadLine().ToLower();
-            if (string.IsNullOrWhiteSpace(address) || string.IsNullOrEmpty(address))
+            form.Address = Console.ReadLine().ToLower();
+            if (string.IsNullOrWhiteSpace(form.Address) || string.IsNullOrEmpty(form.Address))
             {
-                Console.Write("\nInvalid Permanent Address. Field should not be null..!\n\nEnter Your Permanent Address (As Per Govt Id Proof):");
+                Console.Write("\nInvalid Full Address. Field should not be null..!\n\nEnter Your Permanent Address (As Per Govt Id Proof):");
                 goto addressChecker;
             }
-            else if (address.Length <= 20)
+            else if (form.Address.Length <= 20)
             {
-                Console.Write("\n\nEnter Your Full Permanent Address (As Per Govt Id Proof):");
+                Console.Write("\n\nInvalid Address Length...!\nEnter Your Full Permanent Address (As Per Govt Id Proof):");
                 goto addressChecker;
             }
             Console.Write("\n\n===================+-+-+-+-+-+- Deliverion Shipper Account Summary +-+-+-+-+-+-===================\n\n");
             Console.WriteLine("      Shipper Personal Details:     ");
-            Console.Write("\n\nFull Name: {0}", string.Concat(firstName, middleName, lastName));
-            Console.WriteLine("Government Id Name: {0} :  Id: {1}", governmentIdName, governmentId);
-            Console.WriteLine("Age: {0}", ageRange);
-            Console.WriteLine("Email Id: {0}", emailId);
-            Console.WriteLine("Phone Number: {0}", phoneNumber);
-            Console.WriteLine("\nPermanent Address Details: {0}\n\n",address);
+            Console.Write("\n\nFull Name: {0} {1} {2} \n\n",form.FirstName, form.MiddleName, form.LastName);
+            Console.WriteLine("Government Id Name: {0}\n\nGovernment Id: {1}\n", form.SetGovernmentIdName, form.GovernmentId);
+            Console.WriteLine("Age: {0} \n", form.Age);
+            Console.WriteLine("Email Id: {0} \n", form.Email);
+            Console.WriteLine("Phone Number: {0} \n", form.PhoneNumber);
+            Console.WriteLine("Permanent Address Details: {0}\n\n",form.Address);
             Console.WriteLine("         Thank You For Using Our Service             \n\n");
         }
     }
@@ -237,6 +241,7 @@ namespace DeliveryKycConsoleApp
         private string _middleName;
         private string _governmentid;
         private string _governmentidname;
+        private string _setgovernmentidname;
         public string FirstName
         {
             get { return _firstName; }
@@ -283,6 +288,10 @@ namespace DeliveryKycConsoleApp
             get { return _governmentidname; }
             set { _governmentidname = value; }
         }
-
+        public string SetGovernmentIdName
+        {
+            get { return _setgovernmentidname; }
+            set { _setgovernmentidname = value; }
+        }
     }
 }
