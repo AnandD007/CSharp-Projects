508e16aa94ba97da5f6356241c626e46dfc87677
Anand-Dev <64485733+AnandD007@users.noreply.github.com>
2023-02-20 19:30:14 +0530

    Updated program with new modifications
    Updated program file with code modifications as told by my mentor.

--- Assignment-4-String-Operations-Program/Assignment-4-String-Operations-Program/LongestCommonPrefixFinder.cs
+++ Assignment-4-String-Operations-Program/Assignment-4-String-Operations-Program/LongestCommonPrefixFinder.cs
@@ -1,52 +1,52 @@
-﻿using System;
+using System;
 
 namespace StringFunctionsRelatedProblemStatements
 {
     class LongestCommonPrefixFinder
     {
-        internal string[] userInputStringArray = new string[100];
+        internal string[] userInputStringArray = new string[1000];
         internal string userInput = "";
-       internal void longestCommonPrefixFinderFunction()
-	    {
+        internal string LongestCommonPrefixFinderFunction()
+        {
             Console.WriteLine("\n\nEnter any sentence:");
-            userInputChecker:
+        userInputChecker:
             userInput = Console.ReadLine();
-            userInputStringArray = userInput.Split();
-                if (userInputStringArray.Length == 0)
-                {
-                    Console.WriteLine("\n\nField Should Not Be Null..! Enter any Sentence:");
+            userInputStringArray = userInput.Split(' ');
+            if (userInputStringArray.Length == 0)
+            {
+                Console.WriteLine("\n\nField Should Not Be Null..! Enter any Sentence:");
                 goto userInputChecker;
-                    
-                }
-                else if (userInputStringArray.Length == 1)
-                {
-                    Console.WriteLine("Longest Common Prefix: {0}",userInputStringArray[0]);
-                }
-                else
-                {
-                    string shortest = userInputStringArray[0];
-                    foreach(string subString in userInputStringArray)    
-                    {
-                        if (subString.Length < shortest.Length)
-                        {
-                            shortest = subString;
-                        }
-                    }   
-                    for (int i = 0; i < shortest.Length; i++)
-                    {
-                        char singleCharacter = shortest[i];
-                        for (int j = 0; j < userInputStringArray.Length; j++)
-                        {
-                            if (userInputStringArray[j][i] != singleCharacter)
-                            {
-                                // The characters don't match, so return the prefix up to this point
-                                Console.WriteLine("Longest Common Prefix: {0}",shortest.Substring(0, i));
-                            }
-                        }
-                    }
 
+            }
+            else if (userInputStringArray.Length == 1)
+            {
+                return "Longest Common Prefix: "+userInputStringArray[0];
+            }
+            else
+            {
+                string shortest = userInputStringArray[0];
+                foreach (string subString in userInputStringArray)
+                {
+                    if (subString.Length < shortest.Length)
+                    {
+                        shortest = subString;
+                    }
+                }
+                for (int i = 0; i < shortest.Length; i++)
+                {
+                    char singleCharacter = shortest[i];
+                    for (int j = 0; j < userInputStringArray.Length; j++)
+                    {
+                        if (userInputStringArray[j][i] != singleCharacter)
+                        {
+                            // The characters don't match, so return the prefix up to this point
+                            return "Longest Common Prefix: "+ shortest.Substring(0, i);
+                        }
+                        
+                    }
+                }
                 // All strings have the same prefix
-                Console.WriteLine("Longest Common Prefix: {0}", shortest);
+                return "Longest Common Prefix: "+shortest;
             }
 
         }
