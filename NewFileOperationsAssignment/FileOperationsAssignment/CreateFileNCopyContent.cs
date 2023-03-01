d7f5baefc1e9363675e4914ab8c799d6ef2ea50a
AnandD007 <64485733+AnandD007@users.noreply.github.com>
2023-03-01 11:57:55 +0530

    New File Operations Assignment Added to branch

---
+++ NewFileOperationsAssignment/FileOperationsAssignment/CreateFileNCopyContent.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace FileOperationsAssignment
+{
+    internal class CreateFileNCopyContent
+    {
+        internal void FileCreateCopy()
+        {
+            try
+            {
+                string textRead;
+                string fileWriteLocation;
+                string userWriteFileName;
+                string userReadFileName = "";
+                string userFullFileName;
+                string fileLastLine;
+                bool fileFoundStatus = false;
+                string fileReadLocation;
+                string fullWriteFileName;
+
+                // Writing Content in new file
+                Console.Write("\n\nEnter Writing File Location:");
+                fileWriteLocation = Console.ReadLine();
+                while (fileWriteLocation.Length == 0)
+                {
+                    Console.Write("\n\nField Should Not Be Null...!\n\nEnter Writing File Location:");
+                    fileWriteLocation = Console.ReadLine();
+                }
+
+                Console.Write("\n\nEnter Writing File Name:");
+                userWriteFileName = Console.ReadLine().ToLower();
+                while (userWriteFileName.Length == 0)
+                {
+                    Console.Write("\n\nField Should Not Be Null...!\n\nEnter Writing File Name:");
+                    userWriteFileName = Console.ReadLine().ToLower();
+                }
+                while (Path.GetExtension(userWriteFileName).ToLower() != ".txt")
+                {
+                    Console.Write("\n\nInvalid File Extension...! File extension should be (.txt)\n\nEnter Writing File Name:");
+                    userWriteFileName = Console.ReadLine().ToLower();
+                }
+                DirectoryInfo writerFilePath = new DirectoryInfo(@"" + fileWriteLocation);
+                if (writerFilePath == null || !(writerFilePath.Exists))
+                {
+                    throw new DirectoryNotFoundException();
+                }
+                else
+                {
+                    fullWriteFileName = fileWriteLocation + "\\" + userWriteFileName;
+                    FileStream fileWriterObject = new FileStream(@"" + fileWriteLocation + "\\" + userWriteFileName, FileMode.OpenOrCreate, FileAccess.Write);
+                    fileWriterObject.Close();
+                }
+
+                // Read File 
+                Console.Write("\n\nEnter Reading file details:");
+
+                Console.Write("\n\nEnter Reading File Location:");
+                fileReadLocation = Console.ReadLine();
+                while (fileReadLocation.Length == 0)
+                {
+                    Console.Write("\n\nField Should Not Be Null...!\nEnter Reading File Location:");
+                    fileReadLocation = Console.ReadLine();
+                }
+
+                DirectoryInfo readFilePath = new DirectoryInfo(@"" + fileReadLocation);
+                if (readFilePath == null || !(readFilePath.Exists))
+                {
+                    throw new DirectoryNotFoundException();
+                }
+
+                FileInfo[] Files = readFilePath.GetFiles();
+                Console.WriteLine("Files those Presents in your current directory:\n\n");
+
+                // Display the file names
+                int j = 1;
+                foreach (FileInfo i in Files)
+                {
+                    Console.Write("{0}. {1}\n", j++, i.Name.ToLower());
+                }
+                Console.WriteLine("Enter the file Name to read from the above list:");
+                userReadFileName = Console.ReadLine().ToLower();
+                while (userReadFileName.Length == 0)
+                {
+                    Console.Write("\n\nInvalid User Input...!\n\nEnter the file Name to read from the above list:");
+                    userReadFileName = Console.ReadLine().ToLower().ToLower();
+                    string userReadFileExtension = Path.GetExtension(userReadFileName);
+                }
+                while (Path.GetExtension(userReadFileName) != ".txt")
+                {
+                    Console.Write("\n\nInvalid File Extension...! File extension should be (.txt)\n\nEnter Reading File Name:");
+                    userReadFileName = Console.ReadLine().ToLower();
+                }
+                foreach (FileInfo filer in Files)
+                {
+                    if (userReadFileName == filer.Name.ToLower())
+                    {
+                        fileFoundStatus = true;
+                        break;
+                    }
+                    else
+                    {
+                        fileFoundStatus = false;
+                    }
+                }
+                if (fileFoundStatus == true)
+                {
+                    Console.WriteLine("Enter File Name that you want to read:");
+                    userFullFileName = fileReadLocation + "\\" + userReadFileName;
+                    File.Copy(userFullFileName, fullWriteFileName, true);
+                    Console.WriteLine($"{userReadFileName} file text written to {userWriteFileName} file successfully.");
+                }
+                else if (fileFoundStatus == false)
+                {
+                    throw new FileNotFoundException();
+                }
+                else
+                {
+                    throw new UnauthorizedAccessException();
+                }
+            }
+            catch (UnauthorizedAccessException e1)
+            {
+                Console.WriteLine(e1.Message);
+            }
+            catch (FileNotFoundException e2)
+            {
+                Console.WriteLine(e2.Message);
+            }
+            catch (DirectoryNotFoundException e3)
+            {
+                Console.WriteLine(e3.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("\n\nThank You For Using Our Service\n\n");
+            }
+        }
+    }
+}
---
+++ NewFileOperationsAssignment/FileOperationsAssignment/FileOperationsv3.cs
@@ -0,0 +1,77 @@
+﻿using System.Text.RegularExpressions;
+
+namespace FileOperationsAssignment
+{
+    class FileOperationsv3
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("\n1. Create/Open File to Write and Copy the Content from Another File\n\n2. Display all given file content and ask user given word from existing file content to Replace\n\n3. Display Last Line of the given file with it's file name\n\n4. Exit");
+            Console.Write("\n\nEnter the Correct user choice: ");
+            string userChoice = Console.ReadLine();
+            Regex onlyForNumbers = new Regex(@"[\d]$");
+            while(userChoice.Length == 0)
+            {
+                Console.Write("\n\nInput Field should not be null..!\n\nEnter the Correct user choice: ");
+                userChoice = Console.ReadLine();
+            }
+            while(!(onlyForNumbers.IsMatch(userChoice)))
+            {
+                Console.Write("\n\nInvalid Input Entered..!\n\nEnter the Correct user choice: ");
+                userChoice = Console.ReadLine();
+            }
+            while (Convert.ToInt16(userChoice) <= 0 || Convert.ToInt16(userChoice) > 4)
+            {
+                Console.Write("\n\nInvalid Input Entered..!\n\nEnter the Correct user choice: ");
+                userChoice = Console.ReadLine();
+            }
+                switch (userChoice)
+            {
+                case "1":
+                    CreateFileNCopyContent userFileObject = new CreateFileNCopyContent();
+                    userFileObject.FileCreateCopy();
+                    break;
+                case "2":
+                    ReplaceFileContents userFileObject2 = new ReplaceFileContents();
+                    userFileObject2.ReplaceFileContentFunction();
+                    break;
+                case "3":
+                    DisplayLastLine userFileObject3 = new DisplayLastLine();
+                    userFileObject3.DisplayLastNameFunction();
+                    break;
+                case "4":
+                    Console.WriteLine("\nThank You For Using Our Service");
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option Entered");
+                    break;
+            }
+
+        }
+    }
+
+    public class FileNotFoundException : Exception
+    {
+        public void FileNotFoundFunction()
+        {
+            Console.WriteLine("Invalid User Input Entered..!\n\nFile Not Found within the given directory");
+        }
+    }
+
+    public class DirectoryNotFoundException : Exception
+    {
+        public void DirectoryNotFoundFunction()
+        {
+            Console.WriteLine("Invalid User Input Entered..!\n\nGiven directory is Incorrect.");
+        }
+    }
+    public class UnauthorizedAccessException : Exception
+    {
+        public void UnauthorizedAccessFunction()
+        {
+            Console.WriteLine("Invalid User Input Entered..!\n\nUser is not authorized to modify files.");
+        }
+    }
+    
+}
